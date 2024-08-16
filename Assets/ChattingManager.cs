0e51464663f0030775d7ce26831129915e816166
gunggme <ekfrifajfl2019@gmail.com>
2024-08-16 11:04:18 +0900

    채팅 불러오기 채팅 안내려가던 현상 수정

--- Assets/ChattingManager.cs
+++ Assets/ChattingManager.cs
@@ -1,9 +1,9 @@
 using System.Collections;
+using System.Collections.Generic;
 using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class ChattingManager : MonoBehaviour
@@ -11,6 +11,9 @@ public class ChattingManager : MonoBehaviour
     public string characterName;
     public Transform chatParent;
     private string thread_id;
+
+    [TextArea]
+    [SerializeField] private string baseUrl = "https://port-0-girlfriendapi-lzuz68m2a0209e84.sel4.cloudtype.app";
 
     [TextArea]
     [SerializeField] private string serverUrl = "https://port-0-girlfriendapi-lzuz68m2a0209e84.sel4.cloudtype.app/chat";
@@ -22,10 +25,10 @@ public class ChattingManager : MonoBehaviour
 
     public TMP_InputField inputField;
     public Button button;
-    
+    public ScrollRect scrollRect;   
+
     private void Start()
     {
-        //PlayerPrefs.DeleteKey("thread_id");
         if (!PlayerPrefs.HasKey("thread_id"))
         {
             StartCoroutine(GetThread());
@@ -33,7 +36,11 @@ public class ChattingManager : MonoBehaviour
         else
         {
             thread_id = PlayerPrefs.GetString("thread_id");
+            print(thread_id);
+            StartCoroutine(GetMessages()); // 메시지를 가져옵니다.
         }
+
+        button.onClick.AddListener(() => SendMessage(inputField.text));
     }
 
     IEnumerator GetThread()
@@ -48,16 +55,57 @@ public class ChattingManager : MonoBehaviour
         }
         else
         {
-            // Parse the response
             string jsonResponse = request.downloadHandler.text;
             ThreadResponse response = JsonUtility.FromJson<ThreadResponse>(jsonResponse);
 
-            // Store the thread ID
             thread_id = response.thread_id;
             PlayerPrefs.SetString("thread_id", response.thread_id);
             Debug.Log("Thread ID: " + thread_id);
+
+            // Get messages after creating the thread
+            StartCoroutine(GetMessages());
         }
     }
+
+    IEnumerator GetMessages()
+    {
+        string url = baseUrl + "/get_message/" + thread_id;
+
+        UnityWebRequest request = UnityWebRequest.Get(url);
+
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.LogError("Error: " + request.error);
+        }
+        else
+        {
+            string jsonResponse = request.downloadHandler.text;
+            // Use JsonUtility to deserialize JSON array
+            ChatMessageListWrapper wrapper = JsonUtility.FromJson<ChatMessageListWrapper>("{\"messages\":" + jsonResponse + "}");
+            List<ChatMessage> messages = wrapper.messages;
+            messages.Reverse();
+            foreach (ChatMessage message in messages)
+            {
+                print($"{message.role} : {message.content}");
+                if (message.role == "user")
+                {
+                    MyChat text = Instantiate(myChat, chatParent).GetComponent<MyChat>();
+                    text.message.text = FixedText(message.content);
+                }
+                else if (message.role == "assistant")
+                {
+                    IncomingChat text = Instantiate(incomingChatPrefab, chatParent).GetComponent<IncomingChat>();
+                    text.characterName.text = characterName;
+                    text.message.text = FixedText(message.content);
+                }
+            }
+
+            StartCoroutine(FixedToBottom());
+        }
+    }
+
 
     IEnumerator SendMessageToChat(string messageContent)
     {
@@ -68,7 +116,7 @@ public class ChattingManager : MonoBehaviour
         string jsonBody = JsonUtility.ToJson(messageBody);
 
         UnityWebRequest request = new UnityWebRequest(serverUrl, "POST");
-        
+
         byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(jsonBody);
         request.uploadHandler = new UploadHandlerRaw(jsonToSend);
         request.downloadHandler = new DownloadHandlerBuffer();
@@ -86,14 +134,14 @@ public class ChattingManager : MonoBehaviour
             ChatResponse response = JsonUtility.FromJson<ChatResponse>(jsonResponse);
 
             Debug.Log("Received message: " + response.msg);
-            
+
             IncomingChat text = Instantiate(incomingChatPrefab, chatParent).GetComponent<IncomingChat>();
             text.characterName.text = characterName;
             text.message.text = FixedText(response.msg);
 
             inputField.interactable = true;
             button.interactable = true;
-            
+
             StartCoroutine(FixedToBottom());
         }
     }
@@ -103,9 +151,9 @@ public class ChattingManager : MonoBehaviour
         StartCoroutine(SendMessageToChat(message));
 
         MyChat m = Instantiate(myChat, chatParent).GetComponent<MyChat>();
-        
+
         m.message.text = FixedText(message);
-        
+
         inputField.text = "";
         inputField.interactable = false;
         button.interactable = false;
@@ -115,18 +163,17 @@ public class ChattingManager : MonoBehaviour
 
     private IEnumerator FixedToBottom()
     {
-        yield return null;
+        yield return new WaitForSeconds(0.1f);
+        print(scrollRect.verticalNormalizedPosition);
         Canvas.ForceUpdateCanvases();
-    
+
         // 스크롤 위치를 가장 아래로 설정
-        
-        yield return null;
-        
-        RectTransform contentRect = chatParent.GetComponent<RectTransform>();
-        
-        float minDelta = Mathf.Max(0, contentRect.sizeDelta.y - 800);
-        Debug.Log(minDelta);
-        contentRect.anchoredPosition = new Vector2(0, minDelta);
+        scrollRect.verticalNormalizedPosition = 0f;
+        //RectTransform contentRect = chatParent.GetComponent<RectTransform>();
+
+        //float minDelta = Mathf.Max(0, contentRect.sizeDelta.y - 800);
+        //Debug.Log(minDelta);
+        //contentRect.anchoredPosition = new Vector2(0, minDelta);
     }
 
     private string FixedText(string text)
@@ -134,10 +181,10 @@ public class ChattingManager : MonoBehaviour
         string cleanedMessage = text.Replace("\r\n", " ").Replace("\n", " ");
         var newMsg = new StringBuilder();
         int index = 0;
-        
+
         foreach (char c in cleanedMessage)
         {
-            if(index == 0 && c == ' ') continue;
+            if (index == 0 && c == ' ') continue;
             if (index >= 20)
             {
                 newMsg.AppendLine();
@@ -175,3 +222,15 @@ public class MessageBody
     public string msg;
 }
 
+[System.Serializable]
+public class ChatMessage
+{
+    public string role;
+    public string content;
+}
+
+[System.Serializable]
+public class ChatMessageListWrapper
+{
+    public List<ChatMessage> messages;
+}
